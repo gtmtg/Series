db30cd7c68840a321d1e79639c0efe30bde45941
Mihir Garimella <mihir.garimella@gmail.com>
2016-05-25 08:48:43 -0400

    Initial Commit

---
+++ Series/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Series
+//
+//  Created by Mihir Garimella on 5/25/16.
+//  Copyright © 2016 Mihir Garimella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Series/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Series
+//
+//  Created by Mihir Garimella on 5/25/16.
+//  Copyright © 2016 Mihir Garimella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
